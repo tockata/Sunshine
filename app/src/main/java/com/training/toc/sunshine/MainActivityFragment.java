544e1f4b60b50335a4ef518922f67e691914d3f4
Anatoli Angelov <anatoly.angelov@gmail.com>
2016-01-20 21:14:38 +0200

    Added details activity.

--- app/src/main/java/com/training/toc/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/training/toc/sunshine/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.training.toc.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -87,6 +90,16 @@ public class MainActivityFragment extends Fragment {
                 weekForecast);
         ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(detailIntent);
+            }
+        });
 
         return rootView;
     }
